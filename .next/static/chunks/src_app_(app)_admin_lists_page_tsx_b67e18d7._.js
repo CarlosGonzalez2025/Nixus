aef796a00f04f46f47d18f1448352d95eaaf992e
Carlos Gonzalez <info@datenova.io>
2025-11-04 14:34:31 +0000

    dame aqui en el chat las nuevas reglas que debemos aplicar para todo el

--- .next/static/chunks/src_app_(app)_admin_lists_page_tsx_b67e18d7._.js
+++ .next/static/chunks/src_app_(app)_admin_lists_page_tsx_b67e18d7._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_97fe1300._.js"
+  "static/chunks/_34896cdc._.js"
 ],
     source: "dynamic"
 });
