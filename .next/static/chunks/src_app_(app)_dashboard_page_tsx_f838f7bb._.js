2df779340494e17f6b7914f97f83c83ff68ace49
Carlos Gonzalez <info@datenova.io>
2025-11-25 10:06:36 +0000

    porfa valida porque no carga la listas desplegables que vienen del módul

--- .next/static/chunks/src_app_(app)_dashboard_page_tsx_f838f7bb._.js
+++ .next/static/chunks/src_app_(app)_dashboard_page_tsx_f838f7bb._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_ba24c24d._.js"
+  "static/chunks/_85df2974._.js"
 ],
     source: "dynamic"
 });
